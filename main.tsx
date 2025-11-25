7b0c12d19f357b4a9186499e901480948edb1b6b
RobertoSaucedoL <zircksaucedo@gmail.com>
2025-11-25 16:31:15 -0600

    fix: agregar funciones createChatSession y streamMessage

---
+++ main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx"; // usamos tu componente actual
+
+const rootElement = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
